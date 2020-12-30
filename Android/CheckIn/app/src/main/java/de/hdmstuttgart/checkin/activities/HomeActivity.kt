cff9be1c356f34a8fb87f19b730a5a32821c9558
Sascha Wirtz <sw210@hdm-stuttgart.de>
2020-12-30 20:27:46 +0100

    Improved UI
    Further UI work on Homescreen.

--- Android/CheckIn/app/src/main/java/de/hdmstuttgart/checkin/activities/HomeActivity.kt
+++ Android/CheckIn/app/src/main/java/de/hdmstuttgart/checkin/activities/HomeActivity.kt
@@ -1,8 +1,10 @@
 package de.hdmstuttgart.checkin.activities
 
+import android.content.Intent
 import android.nfc.NfcAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import de.hdmstuttgart.checkin.R
 
@@ -13,12 +15,18 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-//        nfcAdapter = android.nfc.NfcAdapter.getDefaultAdapter(this)
 
-//        if(nfcAdapter == null){
-//            Toast.makeText(this, "This device does not support NFC", Toast.LENGTH_SHORT).show()
-//            finish()
-//        }
+        findViewById<Button>(R.id.statistics).setOnClickListener {
+            val intent = Intent(this, StatisticsActivity::class.java)
+            startActivity(intent)
+        }
+
+        nfcAdapter = android.nfc.NfcAdapter.getDefaultAdapter(this)
+
+        if(nfcAdapter == null){
+            Toast.makeText(this, "This device does not support NFC", Toast.LENGTH_SHORT).show()
+            finish()
+        }
 
     }
 }
--- Android/CheckIn/app/src/main/java/de/hdmstuttgart/checkin/activities/StatisticsActivity.kt
+++ Android/CheckIn/app/src/main/java/de/hdmstuttgart/checkin/activities/StatisticsActivity.kt
@@ -1,7 +1,9 @@
 package de.hdmstuttgart.checkin.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import de.hdmstuttgart.checkin.R
 
 class StatisticsActivity : AppCompatActivity() {
