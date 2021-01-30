ec6030ad0d4cc8dc498f7256e2f1de39eb7b063c
TrickyTreat <58302057+TrickyTreat@users.noreply.github.com>
2021-01-30 22:51:05 +0100

    First implementation for dark theme
    Only working for settingsscreen.

--- Android/CheckIn/app/src/main/java/de/hdmstuttgart/checkin/activities/SettingsActivity.kt
+++ Android/CheckIn/app/src/main/java/de/hdmstuttgart/checkin/activities/SettingsActivity.kt
@@ -4,12 +4,54 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CompoundButton
+import android.widget.ImageView
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 import de.hdmstuttgart.checkin.R
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        if (AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkTheme_CheckIn)
+        } else {
+            setTheme(R.style.Theme_CheckIn)
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val themeSwitch: Switch = findViewById(R.id.themeSwitch)
+        if (AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+            themeSwitch.isChecked
+        }
+
+        themeSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                reload()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                reload()
+            }
+        }
+
+//        themeSwitch.setOnCheckedChangeListener(object:CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            fun onCheckedChange(buttonView : CompoundButton, isChecked : Boolean) {
+//                if (isChecked) {
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+//                    reload()
+//                } else {
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+//                    reload()
+//                }
+//            }
+//        }
+//        )
+
+
 
         // button to get back to the home screen
         findViewById<Button>(R.id.backButtonSettings).setOnClickListener {
@@ -17,5 +59,10 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+    private fun reload() {
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
